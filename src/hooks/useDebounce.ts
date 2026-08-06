ff66a945da7a3debb042f296a64078d14bd3ac55
ys-spk <>
2026-08-06 04:17:31 +0000

    make calender generator

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+/** 値の変化を指定 ms だけ遅延させるフック */
+export function useDebounce<T>(value: T, delayMs: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delayMs);
+    return () => clearTimeout(timer);
+  }, [value, delayMs]);
+
+  return debouncedValue;
+}
